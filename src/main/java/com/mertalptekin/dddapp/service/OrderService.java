d5fa36022e90c21b9aed7355bce35bb2d15e8e78
neominal.solutions <neominal.solutions@gmail.com>
2025-09-30 10:21:53 +0300

    Db Settings

--- src/main/java/com/mertalptekin/dddapp/service/OrderService.java
+++ src/main/java/com/mertalptekin/dddapp/service/OrderService.java
@@ -1,7 +1,9 @@
 package com.mertalptekin.dddapp.service;
 
+import com.mertalptekin.dddapp.application.event.OrderSummited;
 import com.mertalptekin.dddapp.service.entity.Order;
 import com.mertalptekin.dddapp.respository.OrderRespository;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 
@@ -9,13 +11,17 @@ import org.springframework.stereotype.Service;
 public class OrderService {
 
     private final OrderRespository orderRespository;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
-    public OrderService(OrderRespository orderRespository) {
+    public OrderService(OrderRespository orderRespository, ApplicationEventPublisher applicationEventPublisher) {
         this.orderRespository = orderRespository;
+        this.applicationEventPublisher = applicationEventPublisher;
     }
 
     public void Save(Order entity){
 
         orderRespository.save(entity);
+        // eventi publish ederek süreci OrderSubmittedListener'a devrettik.
+        this.applicationEventPublisher.publishEvent(new OrderSummited(entity,"Order Submited"));
     }
 }
